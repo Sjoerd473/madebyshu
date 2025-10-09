db4e1b32145383faac61652335f04ece3570c6b4
Sjoerd473 <sjoerdbrussee@hotmail.com>
2025-10-09 19:24:50 +0200

    update pool.js

--- db/pool.js
+++ db/pool.js
@@ -8,3 +8,21 @@ module.exports = new Pool({
     password: process.env.PASSWORD,
     port: Number(process.env.DB_PORT)
 });
+
+
+(async () => {
+  try {
+    const res = await pool.query('SELECT current_database(), current_user');
+    console.log('Connected to DB:', res.rows[0].current_database);
+    console.log('Connected as user:', res.rows[0].current_user);
+  } catch (err) {
+    console.error('DB connection check failed:', err);
+  }
+})();
+
+const tables = await pool.query(`
+  SELECT table_name
+  FROM information_schema.tables
+  WHERE table_schema = 'public'
+`);
+console.log('Available tables:', tables.rows.map(row => row.table_name));
