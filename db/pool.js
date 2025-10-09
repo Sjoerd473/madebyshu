8ae7a5bfd8e7ab7a2f540bf3d8f6fd2c9e0f0c99
Sjoerd473 <sjoerdbrussee@hotmail.com>
2025-10-09 19:21:58 +0200

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -6,5 +6,5 @@ module.exports = new Pool({
     user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
-    port: process.env.DB_PORT
+    port: Number(process.env.DB_PORT)
 });
